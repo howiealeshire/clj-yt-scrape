241838a83b9046b55b0dbdc3abd05f7edca840c4
Howie Aleshire <howie.aleshire@gmail.com>
2021-04-09 16:13:30 -0400

    porting other repo over, directory structure was a mess

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-yt-scrape-port
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_yt_scrape_port/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-yt-scrape-port.core-test
+  (:require [clojure.test :refer :all]
+            [clj-yt-scrape-port.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
